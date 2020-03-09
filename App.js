edfae4444da519fd9aad2143edab5bac4fd7b92a
1997808 <khanhhuyle22@gmail.com>
2020-03-09 09:55:52 +0700

    new StyleSheet

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const
+
+  return (
+    <View style={styles.screen}>
+      <View style={ styles.inputContainer }>
+        <TextInput 
+          placeholder="Course Goal"
+          style={ styles.input }
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD"/>
+      </View>
+      <View />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems:'center'
+  },
+
+  input: {
+    width: '80%', 
+    borderColor:'black',
+    borderWidth: 1
+  }
+});
