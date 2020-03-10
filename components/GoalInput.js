1369a90fa7946075d7320f83117eeb2df975c896
1997808 <khanhhuyle22@gmail.com>
2020-03-10 08:37:55 +0700

    add style

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -26,15 +26,17 @@ const GoalInput = props => {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between', 
+    flex: 1,
+    justifyContent: 'center', 
     alignItems:'center'
   },
 
   input: {
     width: '80%', 
     borderColor:'black',
-    borderWidth: 1
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10
   },
 })
 
